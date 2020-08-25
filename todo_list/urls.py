a259d0163130073a5ae50cc04553dcd865865e69
Laura Petrola <laupetrola@gmail.com>
2020-08-25 17:17:51 -0300

    full to-do list

---
+++ todo_list/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+#Route homepage
+urlpatterns = [
+    path('',views.home, name = 'home'),
+    path('about/',views.about, name = 'about'),
+    path('delete/<list_id>', views.delete, name = 'delete'),
+    path('cross_off/<list_id>', views.cross_off, name = 'cross_off'),
+    path('uncross/<list_id>', views.uncross, name = 'uncross'),
+    path('edit/<list_id>', views.edit, name = 'edit'),
+
+
+
+
+
+    
+]
